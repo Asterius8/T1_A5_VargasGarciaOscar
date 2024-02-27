a03ccb6fb20879d8f197243bbcddbb509d4649e4
Asterius8 <oscar.vg@tecjerez.edu.mx>
2024-02-27 16:52:08 -0600

    Se implemento funcionalidad a lo elementos GUI

--- src/Practica_COMBOBOX.java
+++ src/Practica_COMBOBOX.java
@@ -1,11 +1,17 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.*;
 
-class VentanaPrincipal extends JFrame{
+class VentanaPrincipal extends JFrame implements ActionListener, KeyListener{
 	
 	//Atributos----------------------------------------------------------------------------------------------------------------------------------------------------------------
 	GridBagLayout gbl = new GridBagLayout();
 	GridBagConstraints gbc = new GridBagConstraints();
+	ConversorTemperatura ct = new ConversorTemperatura();
 	
 	JLabel txtConver, txtA, txtIgual;
 	JTextField cajaAConver, cajaResu;
@@ -33,20 +39,159 @@ class VentanaPrincipal extends JFrame{
 		
 		cajaResu = new JTextField();
 		agregarComponente(cajaResu, 3, 1, 1, 1);
+		cajaResu.setEditable(false);
 		
 		comboConver = new JComboBox<String>();
 		agregarComponente(comboConver, 2, 0, 2, 1);
+		comboConver.addItem("Elija una opcion...");
 		comboConver.addItem("°Celsius");
 		comboConver.addItem("°Fahrenheit");
 		comboConver.addItem("°Kelvin");
 		comboConver.addItem("°Rankine");
+		comboConver.addActionListener(this);
 		
 		comboResu = new JComboBox<String>();
 		agregarComponente(comboResu, 1, 1, 1, 1);
-		comboResu.addItem("°Celsius");
-		comboResu.addItem("°Fahrenheit");
-		comboResu.addItem("°Kelvin");
-		comboResu.addItem("°Rankine");
+		comboResu.addItem("Elija una opcion...");
+		
+		cajaAConver.addKeyListener(this);
+		
+		comboConver.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				byte opcion = (byte) comboConver.getSelectedIndex();
+				
+				if (opcion == 1) {
+
+					comboResu.removeAllItems();
+					comboResu.addItem("°Fahrenheit");
+					comboResu.addItem("°Kelvin");
+					comboResu.addItem("°Rankine");
+					comboResu.addActionListener(new ActionListener() {
+						
+						@Override
+						public void actionPerformed(ActionEvent e) {
+
+							byte opcion = (byte) comboResu.getSelectedIndex();
+							
+							if (opcion == 0) {
+								
+								cajaResu.setText( String.valueOf( ct.caF( Double.parseDouble( cajaAConver.getText() ) ) ) );
+								
+							} else if(opcion == 1) {
+
+								cajaResu.setText( String.valueOf( ct.caK( Double.parseDouble( cajaAConver.getText() ) ) ) );
+								
+							}else if(opcion == 2) {
+
+								cajaResu.setText( String.valueOf( ct.caR( Double.parseDouble( cajaAConver.getText() ) ) ) );
+								
+							}
+							
+						}
+					});
+					
+				}else if(opcion == 2) {
+
+					comboResu.removeAllItems();
+					comboResu.addItem("°Celsius");
+					comboResu.addItem("°Kelvin");
+					comboResu.addItem("°Rankine");
+					comboResu.addActionListener(new ActionListener() {
+						
+						@Override
+						public void actionPerformed(ActionEvent e) {
+
+							byte opcion = (byte) comboResu.getSelectedIndex();
+							
+							if (opcion == 0) {
+								
+								cajaResu.setText( String.valueOf( ct.faC( Double.parseDouble( cajaAConver.getText() ) ) ) );
+								
+							} else if(opcion == 1) {
+
+								cajaResu.setText( String.valueOf( ct.faK( Double.parseDouble( cajaAConver.getText() ) ) ) );
+								
+							}else if(opcion == 2) {
+
+								cajaResu.setText( String.valueOf( ct.faR( Double.parseDouble( cajaAConver.getText() ) ) ) );
+								
+							}
+							
+						}
+					});
+					
+				}else if(opcion == 3) {
+
+					comboResu.removeAllItems();
+					comboResu.addItem("°Celsius");
+					comboResu.addItem("°Fahrenheit");
+					comboResu.addItem("°Rankine");
+					comboResu.addActionListener(new ActionListener() {
+						
+						@Override
+						public void actionPerformed(ActionEvent e) {
+
+							byte opcion = (byte) comboResu.getSelectedIndex();
+							
+							if (opcion == 0) {
+								
+								cajaResu.setText( String.valueOf( ct.kaC( Double.parseDouble( cajaAConver.getText() ) ) ) );
+								
+							} else if(opcion == 1) {
+
+								cajaResu.setText( String.valueOf( ct.kaF( Double.parseDouble( cajaAConver.getText() ) ) ) );
+								
+							}else if(opcion == 2) {
+
+								cajaResu.setText( String.valueOf( ct.kaR( Double.parseDouble( cajaAConver.getText() ) ) ) );
+								
+							}
+							
+						}
+					});
+					
+
+				}else if(opcion == 4) {
+					
+					comboResu.removeAllItems();
+					comboResu.addItem("°Celsius");
+					comboResu.addItem("°Fahrenheit");
+					comboResu.addItem("°Kelvin");
+					comboResu.addActionListener(new ActionListener() {
+						
+						@Override
+						public void actionPerformed(ActionEvent e) {
+
+							byte opcion = (byte) comboResu.getSelectedIndex();
+							
+							if (opcion == 0) {
+								
+								cajaResu.setText( String.valueOf( ct.raC( Double.parseDouble( cajaAConver.getText() ) ) ) );
+								
+							} else if(opcion == 1) {
+
+								cajaResu.setText( String.valueOf( ct.raF( Double.parseDouble( cajaAConver.getText() ) ) ) );
+								
+							}else if(opcion == 2) {
+
+								cajaResu.setText( String.valueOf( ct.raK( Double.parseDouble( cajaAConver.getText() ) ) ) );
+								
+							}
+							
+						}
+					});
+					
+				}else {
+					
+					JOptionPane.showMessageDialog(getContentPane(), "Debes elegir una temperatura");
+					
+				}
+				
+			}
+		});
 		
 		pack();
 		setLocationRelativeTo(null);
@@ -66,6 +211,34 @@ class VentanaPrincipal extends JFrame{
 		add(c);
 		
 	}//Metodo
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+		char c = e.getKeyChar();
+		
+		if(c<'0' || c>'9') {
+			
+			e.consume();
+			
+		}
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
 
 
